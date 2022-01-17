70b5fdb5770db5b995ae3a3ea677bad8d51a9860
cressox <73111026+cressox@users.noreply.github.com>
2022-01-17 01:31:13 +0100

    db_connector

--- src/db_connector.java
+++ src/db_connector.java
@@ -1,38 +1,228 @@
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class db_connector {
-    public static void main(String[] args) throws SQLException {
-        String MySQLURL = "jdbc:mysql://localhost:3306/muehle";
-        String databseUserName = "root";
-        String databasePassword = "root";
-        Connection con = null;
-        try {
-            con = DriverManager.getConnection(MySQLURL, databseUserName, databasePassword);
-            if (con != null) {
-                System.out.println("Database connection is successful !!!!");
-            }
-            assert con != null;
+    static String MySQLURL;
+    static String databseUserName;
+    static String databasePassword;
+    static Connection con = null;
 
-            //CallableStatement pst = con.prepareCall("insert into player set id=0");
-            CallableStatement pst = con.prepareCall("delete from player where id=0");
-            pst.execute();
-
-            pst.close();
-            con.close();
-
-            //Statement stmt = con.createStatement();
-            //stmt.executeQuery("insert into 'player' (id, color, stones_in, stones_out) values (0, 'white', 0, 0)");
-            //ResultSet player = stmt.executeQuery("select * from player");
-//            ResultSet game = stmt.executeQuery("SELECT * FROM game");
-            //int id_p = player.getInt("id");
-//            int id_m = game.getInt("id");
-
-            //System.out.println(id_m + "\t" + id_p);
-            //stmt.close();
+    public static void main(String[]args) throws SQLException {
+        open_con("jdbc:mysql://localhost:3306/muehle", "root", "root");
 
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        Map<String, String> tmp_mill = create_game_as_map("1", "4", "5");
+//        change_mill(1, tmp_mill);
+
+//        Map<String, String> tmp_player = create_player_as_map("'1'", "'192.164.178.44'", "'cressox'", "'rte'", null, null, null, "'black'");
+//        change_player(1, tmp_player);
+
+        close_con();
     }
+
+    static void open_con(String db_url, String db_user, String db_user_pw) throws SQLException {
+        MySQLURL = db_url;
+        databseUserName = db_user;
+        databasePassword = db_user_pw;
+        con = DriverManager.getConnection(MySQLURL, databseUserName, databasePassword);
+    }
+
+    static void close_con() throws SQLException {
+        con.close();
+    }
+
+    static void insert_player(String color, int id) throws SQLException {
+        String sql = "INSERT INTO `player` (`id`, `color`, `stones_in`, `stones_out`, `ip`, `pw`, `username`, `online`) VALUES ('" + id + "', '" + color + "', '0', '9', NULL, NULL, NULL, '0')";
+        System.out.println(sql);
+        CallableStatement pst = con.prepareCall(sql);
+        pst.execute();
+        pst.close();
+    }
+
+    static void insert_mill(int id, int id_p1, int id_p2) throws SQLException {
+        insert_player("white", id_p1);
+        insert_player("black", id_p2);
+        String sql = "INSERT INTO `game` (`id`, `running`, `player_one`, `player_two`) VALUES ('" + id + "', '1', '" + id_p1 + "', '" + id_p2 + "')";
+        CallableStatement pst = con.prepareCall(sql);
+        pst.execute();
+        pst.close();
+    }
+
+    static Map get_mill(int mill_id) throws SQLException {
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from game");
+
+        Map<String, String> myResSet = new HashMap();
+
+        while (rs.next()){
+            int id = rs.getInt("id");
+            boolean running = rs.getBoolean("running");
+            int p1 = rs.getInt("player_one");
+            int p2 = rs.getInt("player_two");
+
+            myResSet.put("id", String.valueOf(id));
+            myResSet.put("running", String.valueOf(running));
+            myResSet.put("p1", String.valueOf(p1));
+            myResSet.put("p2", String.valueOf(p2));
+            if (id == mill_id) return myResSet;
+        }
+
+        rs.close();
+        stmt.close();
+        return myResSet;
+    }
+
+    static Map get_player(int player_id) throws SQLException {
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from player");
+
+        Map<String, String> myResSet = new HashMap();
+
+        while (rs.next()){
+            int id = rs.getInt("id");
+            String ip = rs.getString("ip");
+            String pw = rs.getString("pw");
+            String username = rs.getString("username");
+            Boolean online = rs.getBoolean("online");
+            String stones_in = rs.getString("stones_in");
+            String stones_out = rs.getString("stones_out");
+            String color = rs.getString("color");
+
+            myResSet.put("id", String.valueOf(id));
+            myResSet.put("ip", ip);
+            myResSet.put("username", username);
+            myResSet.put("pw", pw);
+            myResSet.put("online", String.valueOf(online));
+            myResSet.put("stones_in", stones_in);
+            myResSet.put("stones_out", stones_out);
+            myResSet.put("color", color);
+            if (id == player_id) return myResSet;
+        }
+
+        rs.close();
+        stmt.close();
+        return myResSet;
+    }
+
+    static void delete_player(int id) throws SQLException {
+        String sql = "delete from player where id=" + id;
+        CallableStatement pst = con.prepareCall(sql);
+        pst.execute();
+        pst.close();
+    }
+
+    static void change_player(int id, Map<String, String> values) throws SQLException {
+        Map<String, String> player_values = get_player(id);
+        // CURRENT VALUES //
+
+        String sql = "update player set " +
+                "id=" + player_values.get("id") +
+                ", ip=" + player_values.get("ip") +
+                ", username=" + player_values.get("username") +
+                ", pw=" + player_values.get("pw") +
+                ", online=" + player_values.get("online") +
+                ", stones_out=" + player_values.get("stones_out") +
+                ", stones_in=" + player_values.get("stones_in") +
+                ", color=" + player_values.get("color") +
+                " where player.id=" + id;
+        // SQL STRING //
+
+        if(values.get("id")!=null) sql = sql.replaceFirst(
+                "id=" + player_values.get("id"),
+                "id=" + values.get("id"));
+
+        if(values.get("ip")!=null) sql = sql.replaceFirst(
+                "ip=" + player_values.get("ip"),
+                "ip=" + values.get("ip"));
+
+        if(values.get("username")!=null) sql = sql.replaceFirst(
+                "username=" + player_values.get("username"),
+                "username=" + values.get("username"));
+
+        if(values.get("pw")!=null) sql = sql.replaceFirst(
+                "pw=" + player_values.get("pw"),
+                "pw=" + values.get("pw"));
+
+        if(values.get("online")!=null) sql = sql.replaceFirst(
+                "online=" + player_values.get("online"),
+                "online=" + values.get("online"));
+
+        if(values.get("stones_in")!=null) sql = sql.replaceFirst(
+                "stones_in=" + player_values.get("stones_in"),
+                "stones_in=" + values.get("stones_in"));
+
+        if(values.get("stones_out")!=null) sql = sql.replaceFirst(
+                "stones_out=" + player_values.get("stones_out"),
+                "stones_out=" + values.get("stones_out"));
+
+        if(values.get("color")!=null) sql = sql.replaceFirst(
+                "color=" + player_values.get("color"),
+                "color=" + values.get("color"));
+        // SETTING OPTIONALLY NEW VALUES //
+
+        System.out.println(sql);
+
+        CallableStatement pst = con.prepareCall(sql);
+        pst.execute();
+        pst.close();
+    }
+
+    static void delete_mill(int id) throws SQLException {
+        String sql = "delete from game where id=" + id;
+        CallableStatement pst = con.prepareCall(sql);
+        pst.execute();
+        pst.close();
+    }
+
+    static void change_mill(int id, Map<String, String> values) throws SQLException {
+        Map<String, String> mill_values = get_mill(id);
+        // CURRENT VALUES //
+
+        String sql = "update game set" +
+                " id=" + mill_values.get("id") +
+                ", player_one=" + mill_values.get("p1") +
+                ", player_two=" + mill_values.get("p2") +
+                " where game.id=" + id;
+        // SQL STRING //
+
+        if(values.get("id")!=null) sql = sql.replaceFirst(
+                "id=" + mill_values.get("id"),
+                "id=" +  values.get("id"));
+
+        if(values.get("p1")!=null) sql = sql.replaceFirst(
+                "player_one=" + mill_values.get("p1"),
+                "player_one=" +  values.get("p1"));
+
+        if(values.get("p2")!=null) sql = sql.replaceFirst(
+                "player_two=" + mill_values.get("p2"),
+                "player_two=" +  values.get("p2"));
+        // SETTING OPTIONALLY NEW VALUES //
+
+        CallableStatement pst = con.prepareCall(sql);
+        pst.execute();
+        pst.close();
+    }
+
+    static Map<String, String> create_player_as_map(String id, String ip, String username, String pw,
+                                             Boolean online, String stones_out, String stones_in, String color){
+        Map<String, String> tmp_map = new HashMap<String, String>();
+        tmp_map.put("id", id);
+        tmp_map.put("ip", ip);
+        tmp_map.put("username", username);
+        tmp_map.put("pw", pw);
+        tmp_map.put("online", online!=null ? String.valueOf(online) : null);
+        tmp_map.put("stones_out", stones_out);
+        tmp_map.put("stones_in", stones_in);
+        tmp_map.put("color", color);
+        return tmp_map;
+    }
+    static Map<String, String> create_game_as_map(String id, String p1, String p2){
+        Map<String, String> tmp_map = new HashMap<String, String>();
+        tmp_map.put("id", id);
+        tmp_map.put("p1", p1);
+        tmp_map.put("p2", p2);
+        return tmp_map;
+    }
+
 }
